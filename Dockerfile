ee689314ddc69635dce03cf3d85d64492bd2fc65
Tonio <toha.futbolist@gmail.com>
2023-01-14 12:57:03 +1000

    Docker & Gitlab-CI

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,12 @@ RUN apt-get upgrade -y
 
 WORKDIR /usr/src/app
 
+ENV PYTHONUNBUFFERED 1
+
+RUN apk update\
+    && apk add postgresql-dev gcc python3-dev musl-dev
+
+RUN pip install --upgrade pip
 COPY .requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . ./mailinglist
--- mailinglist/settings.py
+++ mailinglist/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'mailinglist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mailingList',
-        'USER': 'postgres',
-        'PASSWORD': 'antonio95',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': os.getenv('SQL_ENGINE', "django.db.backends.sqlite3"),
+        'NAME': os.getenv('SQL_DATABASE', os.path.join(BASE_DIR, "db.sqlite3")),
+        'USER': os.getenv('SQL_USER', "user"),
+        'PASSWORD': os.getenv('SQL_PASSWORD', "password"),
+        'HOST': os.getenv('SQL_HOST', "localhost"),
+        'PORT': os.getenv('SQL_PORT', "5432")
     }
 }
 
